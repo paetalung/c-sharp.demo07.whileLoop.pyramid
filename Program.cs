bab9a7a2a8621f255a5e9d316ae87777155f7656
paetalung <paetalung@gmail.com>
2019-06-08 20:30:56 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace demo07.whileLoop.pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many floor? :");
+            int pyramid_floor = int.Parse(Console.ReadLine());
+            string pyramid_print = "";
+
+            while(pyramid_floor > 0)
+            {
+                pyramid_print += "*";
+                Console.WriteLine(pyramid_print);
+                pyramid_floor--;
+            }
+
+
+        }
+    }
+}
